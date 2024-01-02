9c68c7b1e5ca87d04224212fba15b93cf7fdb145
Hüseyin Oğuz Çetinkaya <oguz.cetinkaya@btyon.com>
2024-01-02 11:40:32 +0300

    .

--- src/Components/Tables/Form.js
+++ src/Components/Tables/Form.js
@@ -8,6 +8,8 @@ import DialogActions from '@mui/material/DialogActions';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
+import { TextField } from '@mui/material';
+import axios from 'axios';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
@@ -20,13 +22,26 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
 
 export default function Form() {
   const [open, setOpen] = React.useState(false);
-
+  const [data,setData]=React.useState("");
   const handleClickOpen = () => {
     setOpen(true);
   };
   const handleClose = () => {
     setOpen(false);
+    createData();
   };
+  const createData=()=>{
+  const name = document.getElementById("categoryName").value;
+    axios.post('https://localhost:7053/controller/Create', {
+      name: name
+    })
+    .then(response => {
+      setData(response.data.name)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
 
   return (
     <React.Fragment>
@@ -54,24 +69,11 @@ export default function Form() {
           <CloseIcon />
         </IconButton>
         <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
-            magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
-            ullamcorper nulla non metus auctor fringilla.
-          </Typography>
+        <TextField id="categoryName" label="Category Name" variant="outlined"/><br/>
         </DialogContent>
         <DialogActions>
           <Button autoFocus onClick={handleClose}>
-            Save changes
+            Create
           </Button>
         </DialogActions>
       </BootstrapDialog>
